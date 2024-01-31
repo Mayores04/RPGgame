452cdfc618df196b5d31bbc85cdcd7e0fa98ab9f
BianJake04 <152168832+Mayores04@users.noreply.github.com>
2024-01-31 20:49:27 +0800

    Add files via upload

---
+++ portfolio/MainPort.java
@@ -0,0 +1,10 @@
+package portfolio;
+
+public class MainPort {
+	public static void main(String[] args) {
+
+		Subport Acc = new Subport("Jake J. Mayores" , 18 , "Cainta Rizal" , "System Technology Institute (STI)" , "Ortigas-Cainta");
+		Acc.DisplayInfo();
+		
+	}
+}
